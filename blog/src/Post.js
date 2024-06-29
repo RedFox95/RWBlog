e6d3bed3f213cec570ffeff852d03717b11130b3
Ryan Weidling <ryanweidling@gmail.com>
2024-06-28 21:25:42 -0400

    on submit post clears and resets

--- blog/src/Post.js
+++ blog/src/Post.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Post() {
+    const [postText, setPostText] = useState('');
 
     const handleSubmit = (e) => {
         alert('Post submitted!');
-    }
-
+        setPostText('');
+        e.preventDefault();
+    };
 
     return (
         <div id="postBox">
-            <textarea placeholder="Write your post here!" className="textAreaBox"></textarea>
+            <textarea value={postText} placeholder="Write your post here!" className="textAreaBox" onChange={(e) => setPostText(e.target.value)}></textarea>
             <input type="submit" onClick={handleSubmit}></input>
         </div>
     )
